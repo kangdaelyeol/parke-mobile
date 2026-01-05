b771aa186a9b481e48c92fbe9e241f717fd1a033
rkdeofuf <asa47972000@naver.com>
2026-01-05 23:34:28 +0900

    Feat: add react-native-reanimated package
    react-native-worklets 패키지도 의존성을 위해 반드시 추가가 필요하며, babel config도 Docs에 따라 셋팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+/** @type {import('react-native-worklets/plugin').PluginOptions} */
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [['react-native-worklets/plugin']],
 };
