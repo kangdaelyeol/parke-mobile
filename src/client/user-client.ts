a27acf89e9383e3945a62281d03f0a6c70d8a630
rkdeofuf <asa47972000@naver.com>
2026-02-10 23:55:25 +0900

    Feat: add user client

---
+++ src/client/user-client.ts
@@ -0,0 +1,61 @@
+import { UserDto } from '@/domain/user';
+import { db } from '@/firebaseApp';
+import { convertId } from '@/utils';
+import { get, ref, remove, set, update } from 'firebase/database';
+
+export const userClient = {
+  create: async (dto: UserDto): Promise<boolean> => {
+    const { id, nickname, phone, cardIdList } = dto;
+    const convertedId = id.replaceAll('@', '_').replaceAll(/[\[\].$#]/g, '');
+    try {
+      await set(ref(db, `user/${convertedId}`), {
+        id,
+        nickname,
+        phone,
+        cardIdList,
+      });
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  },
+  update: async (
+    dto: { id: string } & Partial<
+      Pick<UserDto, 'cardIdList' | 'nickname' | 'phone'>
+    >,
+  ): Promise<boolean> => {
+    const convertedId = convertId(dto.id);
+
+    try {
+      await update(ref(db, `user/${convertedId}`), {
+        ...dto,
+      });
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  },
+  getById: async (id: string): Promise<UserDto | null> => {
+    try {
+      const convertedId = convertId(id);
+      const snapShot = await get(ref(db, `user/${convertedId}`));
+      if (!snapShot.exists()) return null;
+      return snapShot.val() as UserDto;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+  deleteById: async (id: string): Promise<boolean> => {
+    try {
+      const convertedId = convertId(id);
+      await remove(ref(db, `user/${convertedId}`));
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  },
+};
