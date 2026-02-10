5f033a57dc213f6bdfc44f4317747787f7e6aeca
rkdeofuf <asa47972000@naver.com>
2026-02-10 23:52:49 +0900

    Fix: change root navigator

--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -1,9 +1,9 @@
-import { useOnBoardContext } from '@/contexts';
 import {
   HomeScreen,
   OnBoardingScreen,
   InitScreen,
   LoginScreen,
+  ProfileScreen,
 } from '@/screens';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // ToDo => move to screens dir with renew
@@ -19,28 +19,22 @@ type RootStackParamList = {
   SearchBLE: undefined;
   ScanComplete: undefined;
   Setting: undefined;
+  Profile: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
-  const { hasSeenOnBoarding } = useOnBoardContext();
-  
-
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {!hasSeenOnBoarding ? (
-        <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
-      ) : (
-        <>
-          <Stack.Screen name="Init" component={InitScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="SearchBLE" component={SearchBLEScreen} />
-          <Stack.Screen name="ScanComplete" component={ScanComplete} />
-          <Stack.Screen name="Setting" component={SettingScreen} />
-        </>
-      )}
+      <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Init" component={InitScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="SearchBLE" component={SearchBLEScreen} />
+      <Stack.Screen name="ScanComplete" component={ScanComplete} />
+      <Stack.Screen name="Setting" component={SettingScreen} />
     </Stack.Navigator>
   );
 }
