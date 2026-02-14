46ac40c29dfdbd12be2151e1e3765d37a78eadac
rkdeofuf <asa47972000@naver.com>
2026-02-14 22:03:21 +0900

    Remove: useProfile VM

--- src/view-model/index.ts
+++ src/view-model/index.ts
@@ -1,4 +1,3 @@
-export * from './use-profile-view-model';
 export * from './use-login-view-model';
 export * from './use-on-boarding-view-model';
 export * from './use-on-boarding-footer-view-model';
