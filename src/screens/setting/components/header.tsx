7dd405d3dbcf3953e0a9c83ea54e470dc55a2b54
rkdeofuf <asa47972000@naver.com>
2026-02-16 15:11:27 +0900

    Feat: add setting header component

---
+++ src/screens/setting/components/header.tsx
@@ -0,0 +1,56 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { FontAwesome6 } from '@react-native-vector-icons/fontawesome6';
+import { useSettingHeaderViewModel } from '@/view-model';
+
+export const Header = () => {
+  const { actions } = useSettingHeaderViewModel();
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Pressable style={styles.backBtn} onPress={actions.backPress}>
+          {({ pressed }) => (
+            <FontAwesome6
+              name="angle-left"
+              iconStyle="solid"
+              size={30}
+              style={[styles.btn, pressed && styles.btnPressed]}
+            />
+          )}
+        </Pressable>
+        <Text style={styles.title}>Settings</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: '#444444',
+    borderBottomWidth: 1,
+  },
+  wrapper: {
+    width: '100%',
+    maxWidth: 360,
+    marginHorizontal: 'auto',
+    paddingBottom: 15,
+  },
+  title: {
+    color: '#eeeeee',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: 500,
+    textAlign: 'center',
+  },
+  backBtn: {
+    position: 'absolute',
+    top: 55,
+    left: 0,
+    zIndex: 3,
+  },
+  btn: {
+    color: '#ebebeb',
+  },
+  btnPressed: {
+    color: '#666',
+  },
+});
