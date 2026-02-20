93ecd3db570f20b8de394240e8053c2b997c8f37
rkdeofuf <asa47972000@naver.com>
2026-02-20 15:44:59 +0900

    feat: add step1 component

---
+++ src/screens/scan-complete/components/step1.tsx
@@ -0,0 +1,53 @@
+import { FocusableInput, PressableButton } from '@/components';
+import { useScanCompleteContext } from '@/contexts/scan-complete-context';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Step1 = () => {
+  const { state, actions } = useScanCompleteContext();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>기본정보를 입력해주세요!</Text>
+      <FocusableInput
+        title="Parke 이름"
+        placeholder="Parke 이름"
+        value={state.name}
+        onChangeText={actions.nameInput}
+      />
+      <FocusableInput
+        title="전화번호"
+        placeholder="전화번호"
+        value={state.phone}
+        onChangeText={actions.phoneInput}
+      />
+      <FocusableInput
+        title="메시지"
+        placeholder="메시지"
+        value={state.message}
+        onChangeText={actions.messageInput}
+      />
+      <PressableButton
+        pressableStyle={styles.btn}
+        onPress={actions.nextPress}
+        title="다음"
+        background={['#222', '#444']}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+  },
+  title: {
+    color: '#eee',
+    fontSize: 25,
+    fontWeight: 700,
+    textAlign: 'center',
+    marginTop: 30,
+  },
+  btn: {
+    marginTop: 10,
+  },
+});
