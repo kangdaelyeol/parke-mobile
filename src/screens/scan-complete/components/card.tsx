665b07cb6b1ff6baf634f65cff93c0eb15e6eba0
rkdeofuf <asa47972000@naver.com>
2026-02-21 23:22:27 +0900

    Fix: change scanComplete card ui

--- src/screens/scan-complete/components/card.tsx
+++ src/screens/scan-complete/components/card.tsx
@@ -10,7 +10,7 @@ type CardProps = {
 
 export const Card = ({ deviceId }: CardProps) => {
   const {
-    state: { name, phone, message },
+    state: { name, phone, message, serial },
   } = useScanCompleteContext();
 
   return (
@@ -18,11 +18,11 @@ export const Card = ({ deviceId }: CardProps) => {
       <View style={styles.bottomBackground} />
       <View style={styles.wrapper}>
         <LogoText width={50} height={45} />
-        <View style={styles.deviceIdLabel}>
-          <Text style={styles.deviceIdTitle}>device ID</Text>
-          <Text style={styles.deviceId}>{deviceId}1923091203120931</Text>
-          <View style={styles.row} />
-          <Text style={styles.deviceIdTitle}>serial</Text>
+        <View style={styles.infoLabel}>
+          <Text style={styles.infoTitle}>device ID</Text>
+          <Text style={styles.infoText}>{deviceId}1923091203120931</Text>
+          <Text style={styles.infoTitle}>serial</Text>
+          <Text style={styles.infoText}>{serial}</Text>
         </View>
         <Text style={styles.message}>{message}</Text>
         <Text style={styles.title}>{name}</Text>
@@ -91,26 +91,20 @@ const styles = StyleSheet.create({
     height: 10,
     borderRadius: '50%',
   },
-  deviceIdLabel: {
+  infoLabel: {
     position: 'absolute',
-    top: 10,
-    right: 10,
+    left: 10,
+    bottom: 10,
     // flexDirection: 'row',
   },
-  deviceIdTitle: {
+  infoTitle: {
     color: '#7b86ff',
     fontSize: 11,
     fontWeight: 500,
   },
-  deviceId: {
+  infoText: {
     color: '#000000',
     fontSize: 10,
     fontWeight: 500,
-    textAlign: 'right',
-  },
-  row: {
-    height: 1,
-    backgroundColor: '#00032d',
-    marginVertical: 2,
   },
 });
