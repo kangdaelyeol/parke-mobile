15143f85d8cb0791eb0f6325b140b116f6114d11
rkdeofuf <asa47972000@naver.com>
2026-02-24 17:56:57 +0900

    Feat: add navigationRef

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from '@/navigation/root-navigator';
@@ -6,36 +6,36 @@ import { useAppController } from '@/controllers/use-app';
 import { BleContextProvider, UserContextProvider } from '@/contexts';
 import { AuthContextProvider } from '@/contexts/auth-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { navigationRef } from '@/navigation/navigation-ref';
 
 function App() {
+  useAppController();
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <AuthContextProvider>
-      <UserContextProvider>
-        <BleContextProvider>
-          <SafeAreaProvider>
-            <StatusBar
-              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            />
-            <GestureHandlerRootView>
-              <AppContent />
-            </GestureHandlerRootView>
-          </SafeAreaProvider>
-        </BleContextProvider>
-      </UserContextProvider>
-    </AuthContextProvider>
+    <GestureHandlerRootView style={styles.handlerView}>
+      <SafeAreaProvider>
+        <NavigationContainer ref={navigationRef}>
+          <AuthContextProvider>
+            <UserContextProvider>
+              <BleContextProvider>
+                <StatusBar
+                  barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                />
+                <RootNavigator />
+              </BleContextProvider>
+            </UserContextProvider>
+          </AuthContextProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
-function AppContent() {
-  useAppController();
-
-  return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
-  );
-}
+const styles = StyleSheet.create({
+  handlerView: {
+    flex: 1,
+  },
+});
 
 export default App;
