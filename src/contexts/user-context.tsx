68c492182b71902382da55bb2c91b182ed5ea698
rkdeofuf <asa47972000@naver.com>
2026-02-24 18:00:53 +0900

    Feat: add syncCards action in useContext

--- src/contexts/user-context.tsx
+++ src/contexts/user-context.tsx
@@ -1,12 +1,25 @@
-import { createContext, PropsWithChildren, useContext, useState } from 'react';
+import { Alert } from 'react-native';
+import {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
 import { CardDto } from '@/domain/card/card-dto';
 import { UserDto } from '@/domain/user/user-dto';
+import { cardService } from '@/services';
+
+const isCardList = (v: any): v is CardDto[] => {
+  return v !== null;
+};
 
 interface UserContextValueType {
   cards: CardDto[];
   user: UserDto;
   setUser: React.Dispatch<React.SetStateAction<UserDto>>;
   setCards: React.Dispatch<React.SetStateAction<CardDto[]>>;
+  syncCardList: () => Promise<void>;
 }
 
 const userContext = createContext({} as UserContextValueType);
@@ -22,8 +35,19 @@ export const UserContextProvider = ({ children }: PropsWithChildren) => {
 
   // temp data
   const [cards, setCards] = useState<CardDto[]>([]);
+
+  const syncCardList = useCallback(async () => {
+    const cardList = await cardService.getList(user.cardIdList || []);
+    if (!isCardList(cardList))
+      return Alert.alert(
+        'uset context - syncCardList: 카드 정보를 불러오는데 실패했습니다.',
+      );
+    setCards(cardList);
+  }, [user.cardIdList]);
   return (
-    <userContext.Provider value={{ cards, user, setCards, setUser }}>
+    <userContext.Provider
+      value={{ cards, user, setCards, setUser, syncCardList }}
+    >
       {children}
     </userContext.Provider>
   );
