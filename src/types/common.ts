7bff0a0a9d60dc630f729eab3cb8a9d359f1507b
rkdeofuf <asa47972000@naver.com>
2026-02-28 22:25:23 +0900

    Feat: add AnimatedViewModel type

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,9 @@ export type ViewModel<S, A> = Readonly<{
   state: Readonly<S>;
   actions: Readonly<A>;
 }>;
+
+type WithAnimated<VM extends object, M> = Readonly<
+  VM & { animated: Readonly<M> }
+>;
+
+export type AnimatedViewModel<S, A, M> = WithAnimated<ViewModel<S, A>, M>;
