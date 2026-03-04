ada22313a927edd36d22cce343cec33dc833c412
rkdeofuf <asa47972000@naver.com>
2026-03-04 14:41:44 +0900

    Feat: cache client
    기존 storage였던 모듈을 client로 변경

--- src/client/cache-client.ts
+++ src/client/cache-client.ts
@@ -1,27 +1,38 @@
 import { MMKV } from 'react-native-mmkv';
 export const kv = new MMKV();
 
+export type AlertPending = null | {
+  phone: string;
+  cardId: string;
+};
+
 export const cacheClient = {
-  lastSeenAt: () => kv.getNumber(`ble:lastSeen`) ?? 0,
-  markSeen: () => kv.set(`ble:lastSeen`, Date.now()),
-  lastDeniedAt: () => kv.getNumber('ble:lastDenied') ?? 0,
-  clearDeniedAt: () => kv.set('ble:lastDenied', 0),
-  markLastDenied: () => kv.set('ble:lastDenied', Date.now()),
-  getPending: () => JSON.parse(kv.getString('ble:pending') ?? 'null'),
-  setPending: (obj: { phone: string; cardId: string }) =>
-    kv.set('ble:pending', JSON.stringify(obj)),
-  clearPending: () => kv.set('ble:pending', 'null'),
-
-  // settings
-  setActive: (val: boolean) => kv.set('setting:active', val),
-  getActive: () => kv.getBoolean('setting:active') ?? null,
-  setNotice: (val: boolean) => kv.set('setting:notice', val),
-  getNotice: () => kv.getBoolean('setting:notice') ?? null,
-  setAutoSet: (val: boolean) => kv.set('setting:autoSet', val),
-  getAutoSet: () => kv.getBoolean('setting:autoSet') ?? null,
-
-  // onBoarding
+  // global
+  getHasInitializedCache: (): boolean => kv.getBoolean('initialized') ?? false,
+  setHasInitializedCache: (setting: boolean) => kv.set('initialized', setting),
   getHasSeenOnBoarding: (): boolean =>
     kv.getBoolean('hasSeenOnBoarding') ?? false,
   setHasSeenOnBoarding: (v: boolean) => kv.set('hasSeenOnBoarding', v),
+
+  // ble device
+  getDeviceSeenAt: (deviceId: string): number =>
+    kv.getNumber(`ble:lastSeen-${deviceId}`) ?? 0,
+  setDeviceSeenAt: (deviceId: string, time: number) =>
+    kv.set(`ble:lastSeen-${deviceId}`, time),
+  getAlertDeniedAt: (): number => kv.getNumber('ble:lastDenied') ?? 0,
+  setAlertDeniedAt: (time: number) => kv.set('ble:lastDenied', time),
+  getAlertPendingList: (): AlertPending[] => {
+    const pending = kv.getString('ble:pending');
+    return pending ? JSON.parse(pending) : [];
+  },
+  setAlertPending: (pendingList: AlertPending[]) =>
+    kv.set('ble:pending', JSON.stringify(pendingList)),
+
+  // setting
+  setActive: (setting: boolean) => kv.set('setting:active', setting),
+  getActive: (): boolean => kv.getBoolean('setting:active') ?? false,
+  setNotice: (setting: boolean) => kv.set('setting:notice', setting),
+  getNotice: (): boolean => kv.getBoolean('setting:notice') ?? false,
+  setAutoSet: (setting: boolean) => kv.set('setting:autoSet', setting),
+  getAutoSet: (): boolean => kv.getBoolean('setting:autoSet') ?? false,
 };
