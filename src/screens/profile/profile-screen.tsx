2b225351c0c2529092c38485a6aee35cb481b287
rkdeofuf <asa47972000@naver.com>
2026-03-04 12:34:36 +0900

    Remove: useProfieVM

--- src/screens/profile/profile-screen.tsx
+++ src/screens/profile/profile-screen.tsx
@@ -1,10 +1,7 @@
 import { StyleSheet, View } from 'react-native';
 import { Main, Header } from '@profile/components';
-import { useProfileViewModel } from '@/view-model';
 
 export default function ProfileScreen() {
-  useProfileViewModel();
-  
   return (
     <View style={styles.container}>
       <Header />
