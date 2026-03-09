6947bd3860ecda6a7e333f9d768c700270f65453
rkdeofuf <asa47972000@naver.com>
2026-03-09 15:31:04 +0900

    UI: change Card Design

--- src/screens/home/components/card.tsx
+++ src/screens/home/components/card.tsx
@@ -1,41 +1,101 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import Animated from 'react-native-reanimated'
-import { LogoText } from '@/assets/logo'
 import { convertPhone } from '@/helpers'
 import { CARD_HEIGHT, CARD_WIDTH } from '@home/constants'
 import { useHomeCardViewModel } from '@/view-model'
 import { HomeCardProps } from '@home/types'
-import { DM_MONO, PRETENDARD } from '@/theme/fonts'
+import { DM_MONO, DM_SANS, PRETENDARD } from '@/theme/fonts'
+import { Loading } from '@/components'
+
+const BOTTOM_HEIGHT = 60
+const TOP_HEIGHT = CARD_HEIGHT - BOTTOM_HEIGHT
+
+const BUTTON_BACKGROUND = '#1c1c1c'
+const BORDER_COLOR = '#2a2a2a'
+const BORDER_COLOR_PRESSED = '#666'
+const FONT_COLOR = '#666'
+const FONT_COLOR_PRESSED = '#f0f0f0'
 
 export const Card = ({ title, phone, idx, message, scan }: HomeCardProps) => {
-  const { animated, actions } = useHomeCardViewModel(idx)
+  const { animated, actions, state } = useHomeCardViewModel(idx)
 
   return (
     <Pressable onPress={actions.cardPress}>
       <Animated.View style={[styles.container, animated.cardStyle]}>
-        <View style={styles.bottomBackground} />
+        {state.loading && <Loading />}
         <View style={styles.wrapper}>
-          <LogoText width={50} height={45} />
-          {/* <FontAwesome6
-            name="ellipsis"
-            iconStyle="solid"
-            size={17}
-            style={styles.ellipsis}
-          /> */}
-          <Text style={styles.message}>{message}</Text>
-          <Text style={styles.title}>{title}</Text>
-          <Text style={styles.phone}>{convertPhone(String(phone))}</Text>
-        </View>
-        <View style={styles.scan}>
-          <View style={[scan ? styles.onIcon : styles.offIcon, styles.icon]} />
-          <Text
-            style={[
-              scan ? styles.scanTextOn : styles.scanTextOff,
-              styles.scanText,
-            ]}
-          >
-            {scan ? '자동감지 On' : '자동감지 Off'}
-          </Text>
+          <View style={styles.topSection}>
+            <Text style={styles.brand}>Parké</Text>
+            <Pressable>
+              {({ pressed }) => (
+                <View style={[styles.more, pressed && styles.optionBtnPressed]}>
+                  <Text style={styles.moreText}>...</Text>
+                </View>
+              )}
+            </Pressable>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.message}>{message}</Text>
+            <Text style={styles.phone}>{convertPhone(String(phone))}</Text>
+            <View style={styles.scan}>
+              <View style={[scan ? styles.dotOn : styles.dotOff, styles.dot]} />
+              <Text
+                style={[
+                  scan ? styles.scanTextOn : styles.scanTextOff,
+                  styles.scanText,
+                ]}
+              >
+                {scan ? '자동감지 On' : '자동감지 Off'}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.divider} />
+          {state.settingCard === -1 && (
+            <View style={styles.bottomSection}>
+              <View style={styles.bottomLeft}>
+                <Pressable onPress={actions.editPress}>
+                  {({ pressed }) => (
+                    <Text
+                      style={[
+                        styles.btn,
+                        styles.optionBtn,
+                        pressed && styles.optionBtnPressed,
+                      ]}
+                    >
+                      수정
+                    </Text>
+                  )}
+                </Pressable>
+                <Pressable onPress={actions.previewPress}>
+                  {({ pressed }) => (
+                    <Text
+                      style={[
+                        styles.btn,
+                        styles.optionBtn,
+                        pressed && styles.optionBtnPressed,
+                      ]}
+                    >
+                      미리보기
+                    </Text>
+                  )}
+                </Pressable>
+              </View>
+              <View style={styles.bottomRight}>
+                <Pressable onPress={actions.deletePress}>
+                  {({ pressed }) => (
+                    <Text
+                      style={[
+                        styles.btn,
+                        styles.deleteBtn,
+                        pressed && styles.deleteBtnPressed,
+                      ]}
+                    >
+                      삭제
+                    </Text>
+                  )}
+                </Pressable>
+              </View>
+            </View>
+          )}
         </View>
       </Animated.View>
     </Pressable>
@@ -47,76 +107,162 @@ const styles = StyleSheet.create({
     marginHorizontal: 'auto',
     width: CARD_WIDTH,
     height: CARD_HEIGHT,
-    backgroundColor: '#1f222b',
+    backgroundColor: '#141414',
     borderRadius: 20,
+    borderWidth: 1,
+    borderColor: BORDER_COLOR,
     overflow: 'hidden',
   },
   wrapper: {
     width: CARD_WIDTH,
     height: CARD_HEIGHT,
-    paddingHorizontal: 10,
+  },
+  topSection: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: TOP_HEIGHT,
+  },
+  brand: {
+    position: 'absolute',
+    top: 15,
+    left: 15,
+    color: FONT_COLOR,
+    fontFamily: DM_SANS.BOLD,
+    textTransform: 'uppercase',
+    letterSpacing: 1.5,
+    fontSize: 11,
   },
   title: {
     position: 'absolute',
-    bottom: 30,
-    right: 10,
-    fontSize: 16,
+    bottom: 35,
+    left: 17,
+    fontSize: 22,
     fontFamily: DM_MONO.MEDIUM,
-    color: '#cccccc',
+    color: '#f0f0f0',
+  },
+  message: {
+    position: 'absolute',
+    bottom: 10,
+    right: 18,
+    fontSize: 12,
+    fontFamily: DM_MONO.MEDIUM,
+    color: FONT_COLOR,
   },
   phone: {
     position: 'absolute',
-    bottom: 10,
-    right: 10,
+    bottom: 25,
+    right: 15,
     fontSize: 14,
-    fontFamily: DM_MONO.REGULAR,
-    color: '#cccccc',
+    fontFamily: DM_MONO.MEDIUM,
+    color: FONT_COLOR,
   },
-  bottomBackground: {
+  ellipsis: { color: FONT_COLOR },
+  divider: {
+    position: 'absolute',
+    width: '100%',
+    height: 1,
+    backgroundColor: BORDER_COLOR,
+    bottom: BOTTOM_HEIGHT,
+  },
+  scan: {
+    position: 'absolute',
+    bottom: 15,
+    left: 18,
+    flexDirection: 'row',
+    gap: 6,
+    alignItems: 'center',
+  },
+  more: {
+    backgroundColor: BUTTON_BACKGROUND,
+    position: 'absolute',
+    top: 13,
+    right: 18,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    borderWidth: 1,
+    borderColor: BORDER_COLOR,
+  },
+  moreText: {
+    color: FONT_COLOR,
+    fontFamily: DM_MONO.MEDIUM,
+    letterSpacing: -3,
+    bottom: 3.5,
+  },
+  dot: {
+    width: 7,
+    height: 7,
+    borderRadius: '50%',
+  },
+  dotOn: {
+    backgroundColor: '#44ff88',
+    boxShadow: '0px 0px 6px #44ff88',
+  },
+  dotOff: {
+    backgroundColor: '#ed3a3a',
+    boxShadow: '0px 0px 6px #fa4c31',
+  },
+  scanText: {
+    fontSize: 12,
+    fontFamily: PRETENDARD.BOLD,
+  },
+  scanTextOn: {
+    color: '#44ff88',
+  },
+  scanTextOff: {
+    color: '#c02b2b',
+  },
+  bottomSection: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    height: 60,
-    backgroundColor: 'rgba(38, 43, 55, 0.51)',
+    height: BOTTOM_HEIGHT,
+    flexDirection: 'row',
   },
-  ellipsis: { color: '#fff', position: 'absolute', right: 15, top: 13 },
-  message: {
-    color: '#616161',
-    position: 'absolute',
-    left: 10,
-    bottom: 65,
-    fontSize: 15,
-  },
-  scan: {
-    position: 'absolute',
-    left: 14,
-    bottom: 12,
+  bottomLeft: {
     flexDirection: 'row',
     gap: 10,
-    alignItems: 'center',
+    flex: 1,
+    paddingLeft: 15,
   },
-  icon: {
-    width: 10,
-    height: 10,
-    borderRadius: '50%',
+  btn: {
+    fontSize: 13,
+    paddingVertical: 9,
+    borderRadius: 300,
+    height: 35,
+    marginVertical: 'auto',
+    fontFamily: DM_SANS.BOLD,
+    backgroundColor: BUTTON_BACKGROUND,
   },
-  onIcon: {
-    backgroundColor: '#00ed08',
-    boxShadow: '0px 0px 5px 2px #299c32',
+  optionBtn: {
+    color: FONT_COLOR,
+    borderWidth: 1,
+    borderColor: BORDER_COLOR,
+    paddingHorizontal: 15,
   },
-  offIcon: {
-    backgroundColor: '#ed3a3a',
-    boxShadow: '0px 0px 5px 2px #fa4c31',
+  optionBtnPressed: {
+    color: FONT_COLOR_PRESSED,
+    borderColor: BORDER_COLOR_PRESSED,
   },
-  scanText: {
-    fontSize: 14,
-    fontFamily: PRETENDARD.MEDIUM,
+  bottomRight: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'flex-end',
   },
-  scanTextOn: {
-    color: '#4fa75c',
+  deleteBtn: {
+    color: '#e05555',
+    right: 15,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: BUTTON_BACKGROUND,
   },
-  scanTextOff: {
-    color: '#c02b2b',
+  deleteBtnPressed: {
+    borderColor: '#e05555',
+    backgroundColor: 'rgba(224,85,85,0.1)',
   },
 })
--- src/view-model/use-home-card-view-model.ts
+++ src/view-model/use-home-card-view-model.ts
@@ -4,13 +4,24 @@ import {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated'
-import { useCardSettingContext, useCardSliderContext } from '@/contexts'
+import {
+  useCardSettingContext,
+  useCardSliderContext,
+  useUserContext,
+} from '@/contexts'
 import { HomeCardViewModel } from '@home/types'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Alert, Linking } from 'react-native'
+import { cardService } from '@/services'
+import { PARKE_WEB_URL } from '@/constants'
 
 export const useHomeCardViewModel = (idx: number): HomeCardViewModel => {
-  const { settingCard } = useCardSettingContext()
+  const { settingCard, cardSettingController } = useCardSettingContext()
   const { selectedCardIdx, sliderController } = useCardSliderContext()
+  const { user, setCards, cards, setUser } = useUserContext()
+  const [loading, setLoading] = useState(false)
+
+  const selectedCard = cards[selectedCardIdx]
   const isSelected = idx === selectedCardIdx
 
   const cardOpacity = useSharedValue(isSelected ? 1 : 0)
@@ -35,15 +46,96 @@ export const useHomeCardViewModel = (idx: number): HomeCardViewModel => {
     }
   })
 
-  const cardPress = () => {
-    if (settingCard !== -1) return
+  const actions = {
+    cardPress: () => {
+      if (settingCard !== -1) return
 
-    if (!isSelected) sliderController.goToIdx(idx)
+      if (!isSelected) sliderController.goToIdx(idx)
+    },
+    editPress: () => {
+      cardSettingController.showSetting(selectedCardIdx)
+    },
+    deletePress: () => {
+      Alert.alert('Parke 삭제', '삭제하시겠습니까? - (재등록 가능)', [
+        {
+          text: '삭제',
+          onPress: async () => {
+            setLoading(true)
+
+            const filteredCardList = cards.filter(
+              card => card.id !== selectedCard.id,
+            )
+            const cardDeleteRes = await cardService.delete(
+              selectedCard.id,
+              user.id,
+            )
+
+            if (!cardDeleteRes) {
+              Alert.alert('오류가 발생했습니다. 다시 시도해주세요.')
+              return setLoading(false)
+            }
+
+            setLoading(false)
+            setCards(filteredCardList)
+            setUser(prev => ({
+              ...prev,
+              cardIdList: filteredCardList.map(c => c.id),
+            }))
+          },
+        },
+        { text: '취소', style: 'cancel' },
+      ])
+    },
+    previewPress: async () => {
+      const url = PARKE_WEB_URL + selectedCard.id
+
+      const supported = await Linking.canOpenURL(url)
+      if (!supported)
+        return Alert.alert('웹 사이트를 열 수 없습니다. 확인해주세요')
+      await Linking.openURL(url)
+    },
+    scanChangePress: async () => {
+      setLoading(true)
+      const res = await cardService.updateScan(
+        selectedCard.id,
+        !selectedCard.scan,
+      )
+      if (!res) {
+        Alert.alert('오류가 발생했습니다. 다시 시도해주세요.')
+        return setLoading(false)
+      }
+      setLoading(false)
+      setCards(prev =>
+        prev.map(card =>
+          card.id !== selectedCard.id
+            ? { ...card }
+            : { ...card, scan: !card.scan },
+        ),
+      )
+    },
+    changePhonePress: async () => {
+      const { phone } = user
+      setLoading(true)
+      const res = await cardService.updatePhone(selectedCard.id, phone)
+      if (!res) {
+        Alert.alert('오류가 발생했습니다. 다시 시도해주세요.')
+        return setLoading(false)
+      }
+      setLoading(false)
+      setCards(prev =>
+        prev.map(card =>
+          card.id !== selectedCard.id ? { ...card } : { ...card, phone },
+        ),
+      )
+    },
   }
 
   return {
-    state: {},
+    state: {
+      loading,
+      settingCard,
+    },
     animated: { cardStyle },
-    actions: { cardPress },
+    actions,
   }
 }
