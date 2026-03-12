423dbdf406467654f480097e981c70a71053f73f
rkdeofuf <asa47972000@naver.com>
2026-03-12 16:48:42 +0900

    Feat: add cache service domain logic for main today dashboard

--- src/services/cache-service.ts
+++ src/services/cache-service.ts
@@ -13,11 +13,38 @@ export const cacheService: CacheService = {
     cacheClient.setAlertPending([])
     cacheClient.setAlertDeniedAt(0)
     cacheClient.setHasInitializedCache(true)
+    cacheClient.setBleScanCount(0)
+    cacheClient.setPhoneChangeCount(0)
+    cacheClient.setToday(new Date().toISOString().slice(0, 10))
   },
   setHasSeenOnBoarding: value => {
     cacheClient.setHasSeenOnBoarding(value)
   },
   getHasSeenOnBoarding: () => {
     return cacheClient.getHasSeenOnBoarding()
+  },
+  getAndSetTodayDashBoard: () => {
+    const cacheToday = cacheClient.getToday()
+    const nowToday = new Date().toISOString().slice(0, 10)
+
+    if (cacheToday !== nowToday) {
+      cacheClient.setBleScanCount(0)
+      cacheClient.setPhoneChangeCount(0)
+      cacheClient.setToday(nowToday)
+    }
+  },
+  increasePhoneChangeCount: () => {
+    const phoneChangeCount = cacheClient.getPhoneChangeCount()
+    cacheClient.setPhoneChangeCount(phoneChangeCount + 1)
+  },
+  increaseBleScanCount: () => {
+    const bleScanCount = cacheClient.getBleScanCount()
+    cacheClient.setBleScanCount(bleScanCount + 1)
+  },
+  getBatteryLevel: () => {
+    return cacheClient.getBatteryLevel()
+  },
+  setBatteryLevel: (level: string) => {
+    cacheClient.setBatteryLevel(level)
   },
 }
