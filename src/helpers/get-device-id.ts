3fe3d2c54b4ecb39e48f5195ee15919dd4d2203a
rkdeofuf <asa47972000@naver.com>
2026-03-14 00:00:45 +0900

    Fix: get device id helper

--- src/helpers/get-device-id.ts
+++ src/helpers/get-device-id.ts
@@ -1,5 +1,5 @@
 import { Buffer } from 'buffer'
 
 export const getDeviceId = (manufacturer: string) => {
-  return Buffer.from(manufacturer, 'base64').toString('utf8').slice(2)
+  return Buffer.from(manufacturer, 'base64').toString('utf8').slice(2, 10) // 8 length
 }
