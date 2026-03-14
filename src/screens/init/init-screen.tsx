a5fc7ac797142d00b89d8c71efecdbcc7de7be7d
rkdeofuf <asa47972000@naver.com>
2026-03-14 23:29:30 +0900

    UI: change init screen

--- src/screens/init/init-screen.tsx
+++ src/screens/init/init-screen.tsx
@@ -1,54 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { LogoIcon, LogoText } from '@/assets/logo'
-import { FocusableInput, Loading, PressableButton } from '@/components'
+import { StyleSheet, View } from 'react-native'
+import { Loading } from '@/components'
 import { useInitViewModel } from '@/view-model'
-import { PRETENDARD } from '@/theme/fonts'
+import { Main, Footer, Header } from './components'
 
 export default function InitScreen() {
-  const { state, actions } = useInitViewModel()
+  const { state } = useInitViewModel()
 
   return (
     <View style={styles.container}>
       {state.loading && <Loading />}
-      <View style={styles.wrapper}>
-        <View style={styles.logoSection}>
-          <LogoIcon width={40} height={40} />
-          <LogoText width={80} height={40} />
-        </View>
-        <Text style={styles.title}>기본 정보를 입력해주세요!</Text>
-        <View>
-          <View style={styles.inputSection}>
-            <FocusableInput
-              title="닉네임"
-              value={state.nickname}
-              onChangeText={actions.nicknameInput}
-              placeholder="닉네임"
-              iconName="my"
-            />
-            <FocusableInput
-              title="연락처"
-              placeholder="연락처"
-              value={state.phone}
-              onChangeText={actions.phoneInput}
-              iconName="phone"
-            />
-          </View>
-          <View style={styles.buttonSection}>
-            <PressableButton
-              title="저장"
-              onPress={actions.savePress}
-              background={['#182a4d', '#223a69']}
-              pressableStyle={styles.pressable}
-            />
-            <PressableButton
-              title="건너뛰기"
-              onPress={actions.skipPress}
-              background={['#161339', '#221d54']}
-              pressableStyle={styles.pressable}
-            />
-          </View>
-        </View>
-      </View>
+      <Header />
+      <Main />
+      <Footer />
     </View>
   )
 }
@@ -58,33 +21,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  wrapper: {
-    width: '100%',
-    maxWidth: 400,
-    marginHorizontal: 'auto',
-    paddingHorizontal: 20,
-  },
-  logoSection: {
-    marginHorizontal: 'auto',
-    flexDirection: 'row',
-    marginTop: 100,
-    gap: 5,
-  },
-  title: {
-    color: '#d3d3d3',
-    marginTop: 25,
-    fontSize: 28,
-    fontFamily: PRETENDARD.MEDIUM,
-    marginHorizontal: 'auto',
-  },
-  inputSection: {
-    marginTop: 30,
-    gap: 20,
-  },
-  buttonSection: {
-    flexDirection: 'row',
-    marginTop: 35,
-    gap: 35,
-  },
-  pressable: { flex: 1, flexShrink: 1, flexBasis: 0 },
 })
