17c5fa64e50fdfef0fa0bb8c4f2bfdec7fc688fe
rkdeofuf <asa47972000@naver.com>
2026-03-14 22:32:49 +0900

    Feat: add color constant

--- src/assets/logo/index.ts
+++ src/assets/logo/index.ts
@@ -1,3 +1,4 @@
 export { default as LogoText } from './logo-text.svg'
 export { default as LogoIcon } from './logo-icon.svg'
 export { default as KakaoLogo } from './kakao-logo.svg'
+export { default as LogoIcon1024 } from './logo-icon-1024.svg'
