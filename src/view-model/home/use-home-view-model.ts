8bb6e7510b91539d459c7b324f2551785adf9689
rkdeofuf <asa47972000@naver.com>
2026-04-16 11:53:54 +0900

    Feat: add appstate active event in home screen
    백그라운드에서 포그라운드로 들어올 시 카드상태 업데이트 추가

--- src/view-model/home/use-home-view-model.ts
+++ src/view-model/home/use-home-view-model.ts
@@ -1,5 +1,5 @@
-import { useCallback, useState } from 'react'
-import { Alert, Linking } from 'react-native'
+import { useCallback, useEffect, useState } from 'react'
+import { Alert, AppState, Linking } from 'react-native'
 import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { useUserContext, useHomeContext } from '@/contexts'
 import { bleService, settingService, permissionService } from '@/services'
@@ -16,6 +16,7 @@ export const useHomeViewModel = () => {
   const { setScanning } = useHomeContext()
   const navigation = useNavigation<HomeStackNavigationProp>()
 
+  // 앱 화면 전환시(다른 스크린에서 Home스크린으로 가는 경우 카드 상태 갱신 및 로그인 상태 체크)
   useFocusEffect(
     useCallback(() => {
       if (!user?.id) return
@@ -94,6 +95,21 @@ export const useHomeViewModel = () => {
       }
     }, [user, cards, setScanning, userContextActions]),
   )
+
+  // active 포그라운드로 돌아오는 경우 카드 상태 갱신
+  useEffect(() => {
+    const subScription = AppState.addEventListener('change', async state => {
+      if (state === 'active') {
+        setLoading(true)
+        await syncCardList()
+        setLoading(false)
+      }
+    })
+
+    return () => {
+      subScription.remove()
+    }
+  }, [syncCardList])
 
   return { state: { loading } }
 }
